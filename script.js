86d789750d2d9299238cf0d9fe810ad0a9e9b6e9
jason <potvinj@gmail.com>
2022-09-19 10:55:26 -0400

    make input feild to change size of grid

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 
-makeSquares(16)
+const defaultSize = 16
+
+makeSquares(defaultSize)
 
 function makeSquares(num) {
     const canvas = document.querySelector('.canvas');
     for (let i = 0; i < num; i++) {
-        let row = document.createElement('div');
+        const row = document.createElement('div');
         row.classList.add(`row${i}`);
         canvas.appendChild(row);
         for (let j = 0; j < num; j++) {
@@ -15,4 +17,33 @@ function makeSquares(num) {
             row.appendChild(column);
         }
     };
+    mouseoverChange();
 };
+
+function mouseoverChange() {
+    let column = document.querySelectorAll(".column");
+    for (let i = 0; i < column.length; i++) {
+        column[i].addEventListener('mouseover', function() {
+            column[i].classList.add('active')
+        })
+    }
+};
+
+function newSize() {
+        //remove existing grid
+    const canvas = document.querySelector('.canvas');
+    let input = document.getElementById('square-input').value;
+    canvas.innerHTML = '';
+        //check if input is less then 100
+    if (input > 100) {
+        alert('sorry input to high choose a smaller number')
+        makeSquares(defaultSize)
+        return;
+    }
+        //if less make new grid
+    makeSquares(input);
+};
+
+//function to check if div is empty
+
+
